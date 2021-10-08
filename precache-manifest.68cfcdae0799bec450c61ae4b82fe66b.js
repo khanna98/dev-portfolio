83f642fa0b566bb032a85915e04905052d1cff55
khanna98 <khanna98@outlook.com>
2021-10-08 06:55:51 +0000

    Deploying to gh-pages from  @ e7cb7593b7648392ccb5de22672c8ff87f3c0500 🚀

---
+++ precache-manifest.68cfcdae0799bec450c61ae4b82fe66b.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ae5dd9db13385f90be125be3577338",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab406c99fec42e0479a3",
+    "url": "/static/css/3.dfb3a24f.chunk.css"
+  },
+  {
+    "revision": "97ced5be2875003abdcf",
+    "url": "/static/css/4.764921a2.chunk.css"
+  },
+  {
+    "revision": "cf929cfb54f06e20184f",
+    "url": "/static/css/main.2a3f8d0f.chunk.css"
+  },
+  {
+    "revision": "4c04b1297b7f90e3c960",
+    "url": "/static/js/2.51636cc5.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.51636cc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab406c99fec42e0479a3",
+    "url": "/static/js/3.0e0705b4.chunk.js"
+  },
+  {
+    "revision": "97ced5be2875003abdcf",
+    "url": "/static/js/4.03bc88fc.chunk.js"
+  },
+  {
+    "revision": "cf929cfb54f06e20184f",
+    "url": "/static/js/main.bba75fcd.chunk.js"
+  },
+  {
+    "revision": "9fc993a58c4f748cade1",
+    "url": "/static/js/runtime-main.c3250943.js"
+  },
+  {
+    "revision": "e0c468ea6fb721055850dcac7d01e76e",
+    "url": "/static/media/14595-thumbs-up.e0c468ea.gif"
+  },
+  {
+    "revision": "4153350315a6d526c0b8b290175991ae",
+    "url": "/static/media/61147-excellent.41533503.gif"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "fdfd8925f8720001ca1662af0b879526",
+    "url": "/static/media/MongoDB-Logo.fdfd8925.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "42f8e219d663d99fbebce545065bdc70",
+    "url": "/static/media/coding-ninja-logo.42f8e219.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "50a8e7f591dea43601c5ba5e3653befa",
+    "url": "/static/media/cu-logo.50a8e7f5.jpeg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "cd061daec047ed1a3ec413ebd806996d",
+    "url": "/static/media/govocal.cd061dae.jpeg"
+  },
+  {
+    "revision": "7c7d143e2625d6332385b5d13f04457c",
+    "url": "/static/media/iit-logo.7c7d143e.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "521444a28d1ac7003ece0c1f030dcd21",
+    "url": "/static/media/node-logo.521444a2.png"
+  },
+  {
+    "revision": "9d19bb098eaf73e8f3f7eb55b302e04e",
+    "url": "/static/media/python-logo.9d19bb09.png"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/static/media/react-logo.638be909.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "1fc43d1e2ed957b90d79b4ddd8f9d889",
+    "url": "/static/media/udacity-logo.1fc43d1e.png"
+  },
+  {
+    "revision": "0341db28274c21e3da41a39c53953804",
+    "url": "/static/media/upgrad-logo.0341db28.png"
+  }
+]);
