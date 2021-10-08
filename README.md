fd476e5f812049bc272008dcb25138907448f1b6
Mayank Khanna <khanna98@outlook.com>
2021-10-08 12:21:10 +0530

    Initial portfolio setup

---
+++ README.md
@@ -0,0 +1,158 @@
+# Mayank Khanna Developer Portfolio ⚡️
+
+## Table of Contents
+- [Mayank Khanna Developer Portfolio ⚡️](#mayank-khanna-developer-portfolio-️)
+  - [Table of Contents](#table-of-contents)
+  - [Portfolio Sections](#portfolio-sections)
+  - [Getting Started](#getting-started)
+    - [Docker Commands](#docker-commands)
+  - [How To Use](#how-to-use)
+  - [Linking Portfolio to Github](#linking-portfolio-to-github)
+  - [Deployment](#deployment)
+      - [Configuring GitHub Actions (Recommended)](#configuring-github-actions-recommended)
+      - [Deploying to Github Pages](#deploying-to-github-pages)
+      - [Deploying to Netlify](#deploying-to-netlify)
+  - [Technologies Used](#technologies-used)
+  - [Illustrations](#illustrations)
+
+## Portfolio Sections
+✔️ Summary and About me\
+✔️ Skills\
+✔️ Education\
+✔️ Work Experience\
+✔️ Open Source Projects Connected with Github\
+✔️ Big Projects\
+✔️ Achievements And Certifications 🏆\
+✔️ Blogs\
+✔️ Talks\
+✔️ Podcast\
+✔️ Contact me\
+✔️ Twitter Timeline\
+✔️ Github Profile
+
+To view a live example, **[click here](/)**.
+
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+You'll need [Git](https://git-scm.com) and [Node.js](https://nodejs.org/en/download/) (which comes with [npm](http://npmjs.com)) installed on your computer or use [Docker](https://www.docker.com/products/docker-desktop).
+
+```
+node@v10.16.0 or higher
+npm@6.9.0 or higher
+git@2.17.1 or higher
+```
+### Docker Commands
+
+```
+1) BUILD IMAGE : docker build -t developerfolio:latest .
+2) RUN IMAGE: docker run -t -p 3000:3000 developerfolio:latest
+```
+
+
+## How To Use 
+
+From your command line, clone and run developerFolio:
+
+```bash
+# Clone this repository
+git clone https://github.com/khanna98/mk-developer-portfolio.git dev-portfolio
+
+# Go into the repository
+cd dev-portfolio
+
+# Setup default environment variables
+
+# For Linux
+cp env.example .env
+# For Windows
+copy env.example .env
+
+# Install dependencies
+npm install
+
+# Start a local dev server
+npm start
+```
+
+## Linking Portfolio to Github
+
+Generate a Github personal access token following these [instructions](https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line) (make sure you don't select any scope just generate a simple token). If you are using [Github Actions](#configuring-github-actions-recommended) to deploy your portfolio you can skip this section.
+
+1. Create a file called .env in the root directory of your project (if not done already in section: [How To Use](#how-to-use))
+
+Note: Configuring environment variables before deploying your portfolio is highly recommended as some components depend on API data. 
+
+```bash
+- dev-portfolio
+  - node_modules
+  - public
+  - src
+  - .env         <-- create it here
+  - env.example  <-- this is the base file
+  - .gitignore
+  - package-lock.json
+  - package.json
+```
+
+2. Inside the .env file, add key `REACT_APP_GITHUB_TOKEN` and assign your github token like this, also add your username as `GITHUB_USERNAME`
+
+```env
+// .env
+REACT_APP_GITHUB_TOKEN = "YOUR GITHUB TOKEN HERE"
+GITHUB_USERNAME = "YOUR GITHUB USERNAME"
+```
+
+Set `showGithubProfile` to true or false to show Contact Profile using Github, defaults to false.
+
+**Warning:** Treat your tokens like passwords and keep them secret. When working with the API, use tokens as environment variables instead of hardcoding them into your programs.
+
+## Deployment
+When you are done with the setup, you should host your website online.
+We highly recommend to read through the [Deploying on Github Pages](https://create-react-app.dev/docs/deployment/#github-pages) docs for React.
+
+#### Configuring GitHub Actions (Recommended)
+First you should enable, Github Actions for the repository you use.
+
+The Profile and the Repository information from Github is only created at the time of deploy and the site needs to be redeployed if those information needs to be updated. So, a configurable [CRON Job](https://docs.github.com/en/actions/reference/events-that-trigger-workflows#scheduled-events) is setup which deploys your site every week, so that once you update your profile on Github it is shown on your portfolio. You can also trigger it manually using `workflow_dispatch` event, see [this guide](https://github.blog/changelog/2020-07-06-github-actions-manual-triggers-with-workflow_dispatch) on how to do that.
+
+- When you are done with the configuration, we highly recommend to read through the [Github Actions Configuring a workflow](https://docs.github.com/en/actions/configuring-and-managing-workflows/configuring-a-workflow) docs.
+
+#### Deploying to Github Pages
+
+This section guides you to deploy your portfolio on Github pages.
+
+- Navigate to `package.json` and enter your domain name instead of `https://developerfolio.js.org/` in `homepage` variable. For example, if you want your site to be `https://<your-username>.github.io/developerFolio`, add the same to the homepage section of `package.json`.
+
+- In short you can also add `/devloperFolio` to `package.json` as both are exactly same. Upon doing so, you tell `create-react-app` to add the path assets accordingly.
+
+- Optionally, configure the domain. You can configure a custom domain with GitHub Pages by adding a `CNAME` file to the `public/` folder.
+
+- Follow through the guide to setup GitHub pages from the official CRA docs [here](https://create-react-app.dev/docs/deployment/#github-pages).
+
+#### Deploying to Netlify
+
+You could also host directly with Netlify by linking your own repository.
+
+[![Deploy To Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/saadpasta/developerFolio)
+
+For more information, read [hosting on Netlify](https://create-react-app.dev/docs/deployment/#netlify).
+
+
+## Technologies Used 
+
+- [React](https://reactjs.org/)
+- [graphql](https://graphql.org/)
+- [apollo-boost](https://www.apollographql.com/docs/react/get-started/)
+- [react-twitter-embed](https://github.com/saurabhnemade/react-twitter-embed)
+- [react-easy-emoji](https://github.com/appfigures/react-easy-emoji)
+- [react-headroom](https://github.com/KyleAMathews/react-headroom)
+- [color-thief](https://github.com/lokesh/color-thief)
+
+## Illustrations
+- [UnDraw](https://undraw.co/illustrations)
+- [Lottie by Oblikweare](https://lottiefiles.com/oblikweare)
+
+---
---
+++ src/portfolio.js
@@ -0,0 +1,448 @@
+/* Change this file to get your personal Portfolio */
+
+// To change portfolio colors globally go to the  _globalColor.scss file
+
+import emoji from "react-easy-emoji";
+
+// Summary And Greeting Section
+
+const illustration = {
+  animated: true // Set to false to use static SVG
+};
+
+const greeting = {
+  username: "Mayank Khanna",
+  title: "Hi all, I'm Mayank",
+  subTitle: emoji(
+    "A passionate software developer, problem solver, and technology enthusiast who loves learning new things. A quick learner with zeal and motivation to succeed, I have experience working as a Full Stack Developer, Voice App Developer, and DevOps Engineer (SRE). I look forward to opportunities that will help me grow and where I can contribute to the growth of the organization with my technical and management skills.",
+  ),
+  resumeLink:
+    "https://drive.google.com/file/d/1sgyqxvshItb2u87qsDx2a3h85xzJlwUF/view?usp=sharing", // Set to empty to hide the button
+  displayGreeting: true // Set false to hide this section, defaults to true
+};
+
+// Social Media Links
+
+const socialMediaLinks = {
+  github: "https://github.com/khanna98",
+  linkedin: "https://www.linkedin.com/in/khanna98/",
+  gmail: "mkhanna98.dev@gmail.com",
+  gitlab: "https://gitlab.com/mayank.khanna.98",
+  // facebook: "https://www.facebook.com/saad.pasta7",
+  // medium: "https://medium.com/@saadpasta",
+  instagram: "https://instagram.com/_mayankkhanna",
+  stackoverflow: "https://stackoverflow.com/users/10852834/mayank-khanna",
+  // Instagram, Twitter and Kaggle are also supported in the links!
+  // To customize icons and social links, tweak src/components/SocialMedia
+  display: true // Set true to display this section, defaults to false
+};
+
+// Skills Section
+
+const skillsSection = {
+  title: "What I do",
+  subTitle: "I build Cloud Infrastructure and Softwares that makes your life easy and work enjoyable.",
+  skills: [
+    emoji(
+      "⚡ Develop highly interactive Front end / User Interfaces for your web and mobile applications"
+    ),
+    emoji("⚡ Setup Cloud Infrastructure on AWS, GCP or any cloud provider."),
+    emoji(
+      "⚡ Manage Cloud Infrastructure using Terraform (IAC)."
+    ),
+    emoji(
+      "⚡ Automate your deployments using CI/CD, Jenkins etc."
+    )
+  ],
+
+  /* Make Sure to include correct Font Awesome Classname to view your icon
+https://fontawesome.com/icons?d=gallery */
+
+  softwareSkills: [
+    {
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
+    },
+    {
+      skillName: "sass",
+      fontAwesomeClassname: "fab fa-sass"
+    },
+    {
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
+    },
+    {
+      skillName: "reactjs",
+      fontAwesomeClassname: "fab fa-react"
+    },
+    {
+      skillName: "nodejs",
+      fontAwesomeClassname: "fab fa-node"
+    },
+    {
+      skillName: "swift",
+      fontAwesomeClassname: "fab fa-swift"
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "gcp",
+      fontAwesomeClassname: "fab fa-google"
+    },
+    {
+      skillName: "aws",
+      fontAwesomeClassname: "fab fa-aws"
+    },
+    {
+      skillName: "firebase",
+      fontAwesomeClassname: "fas fa-fire"
+    },
+    {
+      skillName: "python",
+      fontAwesomeClassname: "fab fa-python"
+    },
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker"
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "fab fa-github"
+    },
+    {
+      skillName: "jenkins",
+      fontAwesomeClassname: "fab fa-jenkins"
+    },
+    {
+      skillName: "Angular",
+      fontAwesomeClassname: "fab fa-angular"
+    },
+    {
+      skillName: "Linux",
+      fontAwesomeClassname: "fab fa-linux"
+    },
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Education Section
+
+const educationInfo = {
+  display: true, // Set false to hide this section, defaults to true
+  schools: [
+    {
+      schoolName: "IIT Guwahati",
+      logo: require("./assets/images/iit-logo.png"),
+      subHeader: "Advanced PG Certification in Cloud & Devops",
+      duration: "September 2021 - Present",
+      desc: "",
+      descBullets: []
+    },
+    {
+      schoolName: "Chitkara University",
+      logo: require("./assets/images/cu-logo.jpeg"),
+      subHeader: "Bachelor of Engineering in Computer Science & Technology",
+      duration: "June 2016 - June 2020",
+      desc: "",
+      descBullets: []
+    }
+  ]
+};
+
+// Your top 3 proficient stacks/tech experience
+
+const techStack = {
+  viewSkillBars: true, //Set it to true to show Proficiency Section
+  experience: [
+    {
+      Stack: "Frontend/Design (React/Angular/React-Native)", //Insert stack or technology you have experience in
+      progressPercentage: "90%" //Insert relative proficiency in percentage
+    },
+    {
+      Stack: "Backend (MongoDB)",
+      progressPercentage: "90%"
+    },
+    {
+      Stack: "AWS/GCP/Cloud Computing",
+      progressPercentage: "75%"
+    },
+    {
+      Stack: "Python",
+      progressPercentage: "85%"
+    },
+    {
+      Stack: "Jenkins/Ansible",
+      progressPercentage: "75%"
+    },
+    {
+      Stack: "Docker",
+      progressPercentage: "80%"
+    },
+    {
+      Stack: "Terraform",
+      progressPercentage: "80%"
+    },
+    {
+      Stack: "Programming",
+      progressPercentage: "60%"
+    }
+  ],
+  displayCodersrank: true // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
+};
+
+// Work experience section
+
+const workExperiences = {
+  display: true, //Set it to true to show workExperiences Section
+  experience: [
+    {
+      role: "Associate Software Engineer",
+      company: "UST BlueConch Technologies Pvt. Ltd.",
+      companylogo: require("./assets/images/bct.jpeg"),
+      date: "August 2020 – Present",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      descBullets: [
+        "Working as the lead in the GCP Migration for Orchestrate Engineering product, where I see and manage the infrastructure that needs to be set up and how to migrate the data and products.",
+        "Helped in solving a critical production issue that saved UPLIGHT $200K per month cost increase",
+        "I wrote a script to add tags to all the instances be it EC2, S3, Lambda, or DynamoDB, which helped us monitor costs better and eventually helped save around $10k per year.",
+        "I also made various monitoring dashboards in Looker and Cloudwatch."
+      ],
+    },
+    {
+      role: "Software Engineer Intern",
+      company: "GoVocal.AI",
+      companylogo: require("./assets/images/govocal.jpeg"),
+      date: "May 201p – June 2020",
+      desc: "I have been working as a Software Developer with in-demand skills and technologies. I have made many Voice Apps and also worked on Mobile Apps using React-Native.",
+      descBullets: [
+        "Developed software lifecycle products from vision through successful deployment.",
+        "Successfully delivered Voice App for Tata Sky on Actions on Google (Google Assistant) and Amazon Alexa (Alexa Skill) into Production.",
+        "Successfully upgraded an android app, Quiddity using React Native. It is a book summaries app where you can read and listen to the summaries of the Great and Best Selling books for free.",
+        "Reviewed, monitored, and repaired any changed code for quality, adherence to standards, and potential performance effects on compatible systems.",
+        "Managed software documentation by testing and validating the accuracy of all new applications to ensure user‐friendliness and productivity.",
+        "Worked with Amazon AWS Lambda and Cloud‐watch for the deployment and testing of the Voice Apps in Production.",
+        "Wrote the request logger for Systematically logging the request and debugging the errors using Winston.",
+        "Designed new software applications by using object‐oriented languages like JavaScript.",
+        "Altered, updated, and enhanced existing systems and applications after performing troubleshooting measures to determine the issue."
+      ]
+    },
+  ]
+};
+
+/* Your Open Source Section to View Your Github Pinned Projects
+To know how to get github key look at readme.md */
+
+const openSource = {
+  showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Some big projects you have worked on
+
+const bigProjects = {
+  title: "Big Projects",
+  subtitle: "SOME STARTUPS AND COMPANIES THAT I HELPED TO CREATE THEIR TECH",
+  projects: [],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Achievement Section
+// Include certificates, talks etc
+
+const achievementSection = {
+  title: emoji("Achievements And Certifications 🏆 "),
+  subtitle:
+    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+
+  achievementsCards: [
+    {
+      title: "Spark Award - UST BlueConch Technologies",
+      subtitle: "Got appreciation for being the spark in the team and motivating them to do better and efficient work.",
+      image: require("./assets/images/14595-thumbs-up.gif"),
+      footerLink: [
+        {name: "Spark Award"},
+        {name: "#Leadership"},
+        {name: "#Appreciation"},
+        {name: "Certificate", url: "https://drive.google.com/file/d/13Efi2NP1QH-PB-tqhV0u-cMc0wQnz1MP/view?usp=sharing"}
+      ]
+    },
+    {
+      title: "Pat on Back - Uplight",
+      subtitle: "Got appreciation for the work I did as a Associate Software Enginner.",
+      image: require("./assets/images/61147-excellent.gif"),
+      footerLink: [
+        {name: "Pat On the Back"},
+        {name: "#Appreciation"},
+        {name: "Certificate 2021", url: "https://drive.google.com/file/d/13Efi2NP1QH-PB-tqhV0u-cMc0wQnz1MP/view?usp=sharing"},
+        {name: "Certificate 2020", url: "https://drive.google.com/file/d/1mConZpXwpeo0j8wZ-X1jtR2SD3aPRo5o/view?usp=sharing"}
+
+      ]
+    },
+    {
+      title: "Python: Zero to Hero",
+      subtitle: "Completed Certifcation from Udemy for Python basics",
+      image: require("./assets/images/python-logo.png"),
+      footerLink: [
+        {name: "Jul 2020 - No Expiration"},
+        {name: "Udemy", url: "https://www.udemy.com"},
+        {name: "Certification Credntials", url: "https://www.udemy.com/certificate/UC-9a058d13-f3a1-4455-aa9c-dd0255ad527c/"}
+      ]
+    },
+    {
+      title: "React: Complete Developer Guide",
+      subtitle: "Completed Certifcation from Udemy for React",
+      image: require("./assets/images/react-logo.png"),
+      footerLink: [
+        {name: "Jun 2020 - No Expiration"},
+        {name: "Udemy", url: "https://www.udemy.com"},
+        {name: "Certification Credntials", url: "https://www.udemy.com/certificate/UC-ec858918-b82d-432f-962c-b7476d87244f/"}
+      ]
+    },
+    {
+      title: "Nodejs: Complete Developer Course",
+      subtitle: "Completed Certifcation from Udemy for Nodejs",
+      image: require("./assets/images/node-logo.png"),
+      footerLink: [
+        {name: "May 2020 - No Expiration"},
+        {name: "Udemy", url: "https://www.udemy.com"},
+        {name: "Certification Credntials", url: "https://www.udemy.com/certificate/UC-add3b90c-f4da-416c-9b92-35c5f14aad66/"}
+      ]
+    },
+    {
+      title: "MongoDB: Complete Developer Guide",
+      subtitle: "Completed Certifcation from Udemy for MongoDB",
+      image: require("./assets/images/MongoDB-Logo.png"),
+      footerLink: [
+        {name: "Apr 2020 - No Expiration"},
+        {name: "Udemy", url: "https://www.udemy.com"},
+        {name: "Certification Credntials", url: "https://www.udemy.com/certificate/UC-f17f0ea5-dcfe-4dfe-8b69-89a30a3d6f93/"}
+      ]
+    },
+    {
+      title: "Data Structures in JAVA",
+      subtitle: "Completed Certifcation from Coding Ninjavs for DS in JAVA ",
+      image: require("./assets/images/coding-ninja-logo.png"),
+      footerLink: [
+        {name: "Jun 2019 - No Expiration"},
+        {name: "Coding Ninjas", url: "https://www.codingninjas.in"},
+        {name: "Certification Credntials", url: "https://www.codingninjas.in/verify/c1e90177b1930cf6"}
+      ]
+    },
+    {
+      title: "Front-end Nanodegree I & II",
+      subtitle: "Completed nanodegrees from Udacity",
+      image: require("./assets/images/udacity-logo.png"),
+      footerLink: [
+        {name: "Mar 2018 - No Expiration"},
+        {name: "Udacity", url: "https://www.udacity.com"},
+        {name: "Certification Credntials I", url: "https://drive.google.com/file/d/1S7NrzYGwVdzE09T9gkIxfOBJOMWBCdBe/view?usp=sharing"},
+        {name: "Certification Credntials II", url: "https://drive.google.com/file/d/1bX-pPJv2xKblK9tSgbkqKI_lH1qwukLK/view?usp=sharing"}
+      ]
+    },
+    {
+      title: "Acadview: Core JAVA Development",
+      subtitle: "Completed training from Acadview (Now upGrad) for basics of JAVA ",
+      image: require("./assets/images/upgrad-logo.png"),
+      footerLink: [
+        {name: "Jun 2019 - No Expiration"},
+        {name: "upGrad", url: "https://www.upgrad.com"},
+        {name: "Certificate Credentials", url: "https://drive.google.com/file/d/1_EfucnNr6j1JUnEQp5Y7ZyEzqgfavIAv/view?usp=sharing"},
+        {name: "Letter of Recommendation", url: "https://drive.google.com/file/d/1PsysrFHeQvoNk_LgJFuuXJUcPlg2ZD1m/view?usp=sharing"}
+      ]
+    },
+  ],
+  display: true // Set false to hide this section, defaults to true
+};
+
+// Blogs Section
+
+const blogSection = {
+  title: "Blogs",
+  subtitle:
+    "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
+
+  blogs: [
+    {
+      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
+      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      description:
+        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+    },
+    {
+      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
+      title: "Why REACT is The Best?",
+      description:
+        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Talks Sections
+
+const talkSection = {
+  title: "TALKS",
+  subtitle: emoji(
+    "I LOVE TO SHARE MY LIMITED KNOWLEDGE AND GET A SPEAKER BADGE 😅"
+  ),
+
+  talks: [
+    {
+      title: "Build Actions For Google Assistant",
+      subtitle: "Codelab at GDG DevFest Karachi 2019",
+      slides_url: "https://bit.ly/saadpasta-slides",
+      event_url: "https://www.facebook.com/events/2339906106275053/"
+    }
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+// Podcast Section
+
+const podcastSection = {
+  title: emoji("Podcast 🎙️"),
+  subtitle: "I LOVE TO TALK ABOUT MYSELF AND TECHNOLOGY",
+
+  // Please Provide with Your Podcast embeded Link
+  podcast: [
+    "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
+  ],
+  display: false // Set false to hide this section, defaults to true
+};
+
+const contactInfo = {
+  title: emoji("Contact Me ☎️"),
+  subtitle:
+    "Discuss a project or just want to say hi? My Inbox is open for all.",
+};
+
+// Twitter Section
+
+const twitterDetails = {
+  userName: "khannamayank98", //Replace "twitter" with your twitter username without @
+  display: false // Set true to display this section, defaults to false
+};
+
+export {
+  illustration,
+  greeting,
+  socialMediaLinks,
+  skillsSection,
+  educationInfo,
+  techStack,
+  workExperiences,
+  openSource,
+  bigProjects,
+  achievementSection,
+  blogSection,
+  talkSection,
+  podcastSection,
+  contactInfo,
+  twitterDetails
+};
