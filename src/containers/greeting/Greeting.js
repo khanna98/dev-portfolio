4f186d52e95769148a2aeab49ad7a1728cc998fa
Mayank Khanna <29047276+khanna98@users.noreply.github.com>
2025-09-03 17:59:49 +0530

    Fixed errors

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -65,7 +65,7 @@ export default function Greeting() {
           <img
             alt="man sitting on table"
             style={{borderRadius: 50}}
-            src={require("../../assets/images/pro-pic.jpg")}
+            src={require("../../assets/images/mayank.jpg")}
           ></img>
           {/* )} */}
         </div>
